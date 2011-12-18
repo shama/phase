6da355d57ce52ea37e6b5c40c58d35ea730ad821
AD7six <andydawson76@gmail.com>
2011-12-18 22:02:35 +0100

    update write task

--- Console/Command/Task/WriteTask.php
+++ Console/Command/Task/WriteTask.php
@@ -1,7 +1,6 @@
 <?php
 
 App::uses('AppShell', 'Console/Command');
-App::uses('Folder', 'Utility');
 
 /**
  * WriteTask
@@ -9,19 +8,47 @@ App::uses('Folder', 'Utility');
 class WriteTask extends AppShell {
 
     /**
+     * template for a new post
+     */
+    protected $template = <<<EOT
+---
+layout: post
+title: %title%
+meta_title: %title%
+meta_description: %title% - update this
+meta_keywords:
+- "a term"
+tags:
+- "a tag"
+---
+
+start typing.
+EOT;
+
+    /**
      * Create a new draft post at todays date
      */
     public function execute() {
-        $slug = date("Y-m-d-") . 'draft-post.md';
+        $title = $this->args[0];
+        $filename = date("Y-m-d-") . Inflector::slug($title) . '.md';
+
+        $path = Configure::read('PhasePosts');
+        $draft = str_replace('%title%', $title, $this->template);
+        file_put_contents($path . $filename, $draft);
+
+        $this->out("$filename created");
     }
 
+    /**
+     * getOptionParser
+     */
 	public function getOptionParser() {
 		$parser = parent::getOptionParser();
         $parser->description(array(
             __d('phase', 'Crete a new draft post at today\'s date'),
         ))->addArgument('title', array(
             'help' => __d('phase', 'The title for the post'),
-            'required' => false,
+            'required' => true
         ));
         return $parser;
 	}
